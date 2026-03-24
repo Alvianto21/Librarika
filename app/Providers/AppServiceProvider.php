59618505882fcdc132255b31baf208ffdf60d84a
Alvianto21 <alviantovinsensius@gmail.com>
2026-03-24 10:37:00 +0700

    change back staging and production for https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment(['production', 'local'])) {
+        if ($this->app->environment(['production', 'staging'])) {
             URL::forceHttps();
         }
     }
