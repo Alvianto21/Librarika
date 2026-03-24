f586dbf9442695dbb464182b091f0b1ac230f1d7
Alvianto21 <alviantovinsensius@gmail.com>
2026-03-24 10:18:32 +0700

    change staging env to local env in https testing

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->environment(['production', 'staging'])) {
+        if ($this->app->environment(['production', 'local'])) {
             URL::forceHttps();
         }
     }
